c5254ed83e4aa17573bb442d6dc7210a67b099fc
kioria-samuel <msomikioria@gmail.com>
2022-03-29 22:24:12 +0300

    relayed_dta _rom the identified asset to front end

--- identify.php
+++ identify.php
@@ -2,6 +2,7 @@
 // Include config file
 require_once("config.php") ;
 $assetno=$status='';
+$assetno=$assetname=$model='';
 if(isset($_POST['submit'])){
 // echo 'there is something';
  // Prepare a select statement
@@ -20,6 +21,14 @@ if(isset($_POST['submit'])){
          mysqli_stmt_store_result($stmt);
          
          if(mysqli_stmt_num_rows($stmt)  >0){
+          // $bind=mysqli_stmt_bind_result($stmt,$assetno,$assetname,$model);
+          // $result=mysqli_fetch_row($bind);
+          // print_r($result);
+          $stmt->bind_result($assetno,$assetname,$model);
+          $stmt->fetch();
+          // print_r($assetno);
+          // print_r($assetname);
+          // print_r($model);
             
             $status="component registered";
            // $assets=mysqli_fetch_all($stmt,MYSQLI_ASSOC);
@@ -34,28 +43,9 @@ if(isset($_POST['submit'])){
      // Close statement
      mysqli_stmt_close($stmt);
  }
-// //construct query
-// $sql='SELECT  asset_no,asset_name,model FROM assets WHERE  asset_no=?';
-// // Bind variables to the prepared statement as parameters
-// $sql=mysqli_stmt_bind_param($sql, "i", $assetno);
-//    //set the parameter
-//    $assetno=$_POST['assetno'];         
-// //make the query
-// $result=mysqli_query($con,$sql);
-// $rowcount=mysqli_num_rows($result);
-// if($rowcount>0){
-// //fetch the results from the query as an array
-// $assets=mysqli_fetch_all($result,MYSQLI_ASSOC);
-// print_r($assets);
-// }else{
-//   echo 'no such record boy component unregistered';
-// }
-//ree thersult o query rom memory
-// mysqli_free_result($result);
-// //close connection
-// mysqli_close($con);
+
 }else{
-  echo 'data not sent ';
+  $status="please scan";
 }
 
 
@@ -73,27 +63,27 @@ if(isset($_POST['submit'])){
             <div class="form-group row">
               <button type="submit" name="submit" class="btn btn-success col-sm-2  "><i class="fa fa-barcode"></i>|scan</button>
               <div class="col-sm-10">
-                <input type="text" class="form-control" name ="assetno" id="scannedbarcode" placeholder="input_from_scanner">
+                <input type="text" class="form-control"  value="<?php echo $assetno?>" name ="assetno" id="scannedbarcode" placeholder="input_from_scanner">
                 <div class="text-danger"><?php echo $status;?></div>
               </div>
             </div>
-            <!-- <div class="form-group row">
+             <div class="form-group row">
               <label for="inputasset" class="col-sm-2 col-form-label">Asset</label>
               <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputasset" placeholder="assset name">
+                <input type="text" class="form-control" value="<?php echo $assetname?>" id="inputasset" placeholder="assset name">
               </div>
             </div>
             <div class="form-group row">
               <label for="inputmodel" class="col-sm-2 col-form-label">Model</label>
               <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputmodel" placeholder="inputmodel">
+                <input type="text" value="<?php echo $model?>" class="form-control" id="inputmodel" placeholder="inputmodel">
               </div>
-            </div> -->
+            </div> 
             
             <div class="form-inline-block">
 
               <!-- <button type="save" class="btn btn-success col-sm-3  "><i class="fa fa-floppy-o"></i>|save</button> -->
-              <button type="verify"  id="types" class="btn btn-success col-sm-2"><a href="#"></a><i class="fa fa-plus"></i>|add</button>
+              <button type="verify"  id="types" class="btn btn-success col-sm-2"><a href="identify.php"></a><i class="fa fa-plus"></i>|reset</button>
               <!-- <button type="scan" class="btn btn-success col-sm-3  "><i class="fa fa-trash"></i>|delete</button> -->
   
             </div>
